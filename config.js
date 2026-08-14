c7aa4a427318961c057d1d36c009062c631bab2c
loyal std <164795624+loyal-studios@users.noreply.github.com>
2026-08-15 04:49:44 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 window.LSM_CONFIG = {
+  title: 'Loyal Studios Management',
   appUrl: 'https://script.google.com/macros/s/AKfycbxH5X3vifl26ox852UyONsa69ZRns3h7qRm0iFVNsjITsuslsbs6zYEdmnAo_KuZq92/exec',
-  logoUrl: './assets/logo.png',
-  faviconUrl: './assets/favicon.png'
+  logoDarkUrl: './assets/logo-dark.png',
+  logoLightUrl: './assets/logo-light.png',
+  faviconUrl: './assets/favicon.png',
+  appIconUrl: './assets/app-icon.png',
+  themeColorDark: '#0b0b0c',
+  themeColorLight: '#f7f7f5',
+  slowLoadMs: 15000
 };
