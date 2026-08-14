f7005f12b168033ceb7ee69a2d31fcfdfdc34bac
loyal std <164795624+loyal-studios@users.noreply.github.com>
2026-08-14 09:05:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LSM_CONFIG = {
-  appUrl: 'https://script.google.com/macros/s/AKfycbxH5X3vjifl26ox852UyONsa69ZRns3h7qRm0iFVNsjlTsuslbs6zYEdmnAo_KuZq92/exec',
+  appUrl: 'https://script.google.com/macros/s/AKfycbxH5X3vifl26ox852UyONsa69ZRns3h7qRm0iFVNsjITsuslsbs6zYEdmnAo_KuZq92/exec',
   logoUrl: './assets/logo.png',
   faviconUrl: './assets/favicon.png'
 };
